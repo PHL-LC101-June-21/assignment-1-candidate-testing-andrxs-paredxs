db36406c150c137f014f25ff681d32c188756e5b
andrxs <44853512+andrxs-paredxs@users.noreply.github.com>
2021-06-17 22:53:51 +0000

    Completed Part 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -15,19 +15,23 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  return input.question("What's your name?\n");
 }
 
-function askQuestion() {
+function askQuestion(question) {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  return input.question(question + "\n")
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+  if ( candidateAnswer.toLowerCase() === correctAnswer.toLowerCase() ){
+    console.log("Awesome correct answer");
+  } else {
+    console.log("I'm sorry wrong answer");
+  }
 
   let grade;
   
@@ -36,10 +40,10 @@ function gradeQuiz(candidateAnswers) {
 }
 
 function runProgram() {
-  askForName();
+  candidateName = askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
-  askQuestion();
+  console.log(`Welcome ${candidateName.toUpperCase()} to the test for future Austronats`);
+  candidateAnswer = askQuestion(this.question);
   gradeQuiz(this.candidateAnswers);
 }
 
